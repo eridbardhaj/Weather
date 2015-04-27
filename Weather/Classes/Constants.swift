965ec6a070fd3c7cda759cb4606c995dd49196e6
Erid <erid.bardhaj@gmail.com>
2015-04-27 22:26:11 +0200

    Added configuration files

---
+++ Weather/Classes/Constants.swift
@@ -0,0 +1,35 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Erid Bardhaj on 4/27/15.
+//  Copyright (c) 2015 STRV. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject
+{
+    class Colors
+    {
+        class func blackColor() -> UIColor
+        {
+            return UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
+        }
+        
+        class func lightBlue() -> UIColor
+        {
+            return UIColor(red: 47/255, green: 145/255, blue: 255/255, alpha: 1)
+        }
+        
+        class func orangeColor() -> UIColor
+        {
+            return UIColor(red: 47/255, green: 136/255, blue: 71/255, alpha: 1)
+        }
+        
+        class func whiteColor() -> UIColor
+        {
+            return UIColor.whiteColor()
+        }
+    }
+}
